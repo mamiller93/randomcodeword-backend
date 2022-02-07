ed95d7a6ef1fb63c081b3c25e2437011587d22a1
Matthew Miller <mamiller93@gmail.com>
2022-02-07 03:18:36 -0500

    CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 import os
 
-from flask import Flask
+from flask import request, Flask, make_response, jsonify
+from flask_cors import CORS, cross_origin
 from random_word import RandomWords
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
+    cors = CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -27,8 +31,21 @@ def create_app(test_config=None):
 
     # a simple page that says hello
     @app.route('/get')
+    @cross_origin()
     def get():
+        params = request.args
         r = RandomWords()
-        return r.get_random_word()
+        words = None
+        while words == None:
+            words = r.get_random_words(hasDictionaryDef=True,
+                                       minCorpusCount=2,
+                                       includePartOfSpeech=params.get(
+                                           "partOfSpeech") or "noun,verb",
+                                       limit=params.get('limit') or 1)
+
+        responseObject = {
+            "words": words,
+        }
+        return make_response(jsonify(responseObject)), 201
 
     return app
